79c313bdcbde5a21a710e862a007c5eebc20c2dc
madlee08 <maikemai12@hotmail.com>
2020-10-31 16:20:05 -0600

    renombrar variables. metodos ubicar y rotar barcos

--- cliente.py
+++ cliente.py
@@ -18,18 +18,34 @@ def centrar_hztl(dimensionX_pantalla, dimensionX_imagen):
 class administrador_de_ventanas():
     def __init__(self):
         self.ventana = 'menu'
-        self.boton_jugar = pygame.image.load("./assets/botones/jugar.png")
-        self.boton_instr = pygame.image.load("./assets/botones/instrucciones.png")
-        self.boton_salir = pygame.image.load("./assets/botones/salir.png")
-        self.boton_regresar = pygame.image.load("./assets/botones/regresar.png")
-
+        self.btn_jugar = pygame.image.load("./assets/botones/jugar.png")
+        self.btn_instr = pygame.image.load("./assets/botones/instrucciones.png")
+        self.btn_salir = pygame.image.load("./assets/botones/salir.png")
+        self.btn_regsr = pygame.image.load("./assets/botones/regresar.png")
         self.tablero = pygame.image.load("./assets/tablero/tablero_juego.png")
+        self.barco0 = pygame.image.load("./assets/barcos/0_x2.png")
 
-        self.barco1_x2 = pygame.image.load("./assets/barcos/1_x2.png")
-        self.rot1 = False
+        self.dX_boton = 200
+        self.dY_boton = 50
+        
+        self.pX_tablero = 50
+        self.pY_tablero = 50
 
-        self.dimX_boton = 200
-        self.dimY_boton = 50
+        self.pX_bmen = centrar_hztl(dimX_vent, self.dX_boton)
+        self.pY_jugar = 300
+        self.pY_instr = 400
+        self.pY_salir = 500
+
+        self.pX_regsr = 1000
+        self.pY_regsr = 600
+        
+        self.pX_barco = [50, 0, 0, 0, 0]
+        self.pY_barco = [50, 0, 0, 0, 0]
+        
+        self.dX_barco = [93, 0, 0, 0, 0]
+        self.dY_barco = [45, 0, 0, 0, 0]
+
+        self.rotado_barco = [False, False, False, False, False]
 
         self.negro = (0,0,0)
 
@@ -55,65 +71,61 @@ class administrador_de_ventanas():
         self.salir_x()
 
         #posición X de los botones del menú
-        posX = centrar_hztl(dimX_vent, self.dimX_boton)
-
-        posY_jugar = 300
-        posY_instr = 400
-        posY_salir = 500
 
         pantalla.fill(self.negro)
         #agrega las imágenes previamente cargadas al menú
-        pantalla.blit(self.boton_jugar, (posX, posY_jugar))
-        pantalla.blit(self.boton_instr, (posX, posY_instr))
-        pantalla.blit(self.boton_salir, (posX, posY_salir))
+        pantalla.blit(self.btn_jugar, (self.pX_bmen, self.pY_jugar))
+        pantalla.blit(self.btn_instr, (self.pX_bmen, self.pY_instr))
+        pantalla.blit(self.btn_salir, (self.pX_bmen, self.pY_salir))
 
         pygame.display.update()
         if pygame.mouse.get_pressed()[0] == True:
-            if posX <= pygame.mouse.get_pos()[0] <= posX + self.dimX_boton:
-                if posY_jugar <= pygame.mouse.get_pos()[1] <= posY_jugar + self.dimY_boton:
+            if self.pX_bmen <= pygame.mouse.get_pos()[0] <= self.pX_bmen + self.dX_boton:
+                if self.pY_jugar <= pygame.mouse.get_pos()[1] <= self.pY_jugar + self.dY_boton:
                     self.ventana = 'preparacion'
 
-                if posY_salir <= pygame.mouse.get_pos()[1] <= posY_salir + self.dimY_boton:
+                if self.pY_salir <= pygame.mouse.get_pos()[1] <= self.pY_salir + self.dY_boton:
                     self.salir_menu()
 
     def preparacion(self):
         self.salir_x()
 
         pantalla.fill(self.negro)
-        
-        posX_regresar = 1000
-        posY_regresar = 600
 
-        pantalla.blit(self.boton_regresar, (posX_regresar, posY_regresar))
-        pantalla.blit(self.tablero, (50, 50))
-        
-        if self.rot1 == True:
-            pantalla.blit(pygame.transform.rotate(self.barco1_x2, 90), (50,50))
-            pygame.display.update()
-
-            if pygame.mouse.get_pressed()[0] == True:
-                if 50 <= pygame.mouse.get_pos()[0] <= 100:
-                    if 50 <= pygame.mouse.get_pos()[1] <= 100:
-                        self.rot1 = False
-                        time.sleep(0.25)
-            
-        else:
-            pantalla.blit(self.barco1_x2, (50,50))
-            pygame.display.update()
-
-            if pygame.mouse.get_pressed()[0] == True:
-                if 50 <= pygame.mouse.get_pos()[0] <= 100:
-                    if 50 <= pygame.mouse.get_pos()[1] <= 100:
-                        self.rot1 = True
-                        time.sleep(0.25)
-            
+        pantalla.blit(self.btn_regsr, (self.pX_regsr, self.pY_regsr))
+        pantalla.blit(self.tablero, (self.pX_tablero, self.pY_tablero))
+        self.ubicarbarcos()
+        self.rotarbarcos()
 
         if pygame.mouse.get_pressed()[0] == True:
-            if posX_regresar <= pygame.mouse.get_pos()[0] <= posX_regresar + self.dimX_boton:
-                if posY_regresar <= pygame.mouse.get_pos()[1] <= posY_regresar + self.dimY_boton:
+            if self.pX_regsr <= pygame.mouse.get_pos()[0] <= self.pX_regsr + self.dX_boton:
+                if self.pY_regsr <= pygame.mouse.get_pos()[1] <= self.pY_regsr + self.dY_boton:
                     self.ventana = 'menu'
 
-       
+    def ubicarbarcos(self):
+        if self.rotado_barco[0] == True:
+            rotado = pygame.transform.rotate(self.barco0, 90)
+            pantalla.blit(rotado, (self.pX_barco[0], self.pY_barco[0]))
+            pygame.display.update()
+
+        else:
+            pantalla.blit(self.barco0, (self.pX_barco[0], self.pY_barco[0]))
+            pygame.display.update()
+
+    def rotarbarcos(self):
+        if pygame.mouse.get_pressed()[2] == True:
+            if self.rotado_barco[0] == True:
+                if self.pX_barco[0] <= pygame.mouse.get_pos()[0] <= self.pX_barco[0] + self.dY_barco[0]:
+                    if self.pY_barco[0] <= pygame.mouse.get_pos()[1] <= self.pY_barco[0] + self.dX_barco[0]:
+                        self.rotado_barco[0] = False
+                        time.sleep(0.25)
+
+            else:
+                if self.pX_barco[0] <= pygame.mouse.get_pos()[0] <= self.pX_barco[0] + self.dX_barco[0]:
+                    if self.pY_barco[0] <= pygame.mouse.get_pos()[1] <= self.pY_barco[0] + self.dY_barco[0]:
+                        self.rotado_barco[0] = True
+                        time.sleep(0.25)
+
 #programa principal
 ventana = administrador_de_ventanas()
 while True:
