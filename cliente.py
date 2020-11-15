6dabd0f851b9b27ab7bed636dde2d0b79a1d2822
madlee08 <maikemai12@hotmail.com>
2020-11-14 20:49:28 -0600

    adicion fase multi -metodo revisar celda

--- cliente.py
+++ cliente.py
@@ -34,11 +34,11 @@ class administrador_de_botones:
         self.pY_instr = 400
         self.pY_salir = 500
 
-        self.pX_regsr = 1000
-        self.pY_regsr = 600
+        self.pX_regsr = 1050
+        self.pY_regsr = 650
 
-        self.pX_bscar = 750
-        self.pY_bscar = 600
+        self.pX_bscar = 800
+        self.pY_bscar = 650
 
     def menu(self):
         pantalla.blit(self.btn_jugar, (self.pX_bmen, self.pY_jugar))
@@ -72,6 +72,23 @@ class administrador_de_botones:
             if self.pX_regsr <= pX_mouse <= self.pX_regsr + self.dX_boton:
                 if self.pY_regsr <= pY_mouse <= self.pY_regsr + self.dY_boton:
                     self.salida = 'menu'
+            
+            if self.pX_bscar <= pX_mouse <= self.pX_bscar + self.dX_boton:
+                if self.pY_bscar <= pY_mouse <= self.pY_bscar + self.dY_boton:
+                    self.salida = 'juego'
+
+    def juego(self):
+        pantalla.blit(self.btn_regsr, (self.pX_regsr, self.pY_regsr))
+
+        pX_mouse = pygame.mouse.get_pos()[0]
+        pY_mouse = pygame.mouse.get_pos()[1]
+        clic_izq = pygame.mouse.get_pressed()[0]
+
+        if clic_izq == True:
+            if self.pX_regsr <= pX_mouse <= self.pX_regsr + self.dX_boton:
+                if self.pY_regsr <= pY_mouse <= self.pY_regsr + self.dY_boton:
+                    self.salida = 'preparacion'
+                    time.sleep(0.2)
 
     def instrucciones(self):
         pantalla.blit(self.btn_regsr, (self.pX_regsr, self.pY_regsr))
@@ -88,7 +105,8 @@ class administrador_de_botones:
 
 class administrador_de_imagenes:
     def __init__(self):
-        self.tablero = pygame.image.load("./assets/tablero/tablero_juego.png")
+        self.tablero_a = pygame.image.load("./assets/tablero/tablero_azul.png")
+        self.tablero_b = pygame.image.load("./assets/tablero/tablero_blanco.png")
         self.ayuda = pygame.image.load("./assets/texto/ayuda.png")
         self.instr = pygame.image.load("./assets/texto/instrucciones.png")
         self.pX_tablero = 50
@@ -105,8 +123,13 @@ class administrador_de_imagenes:
     
     def preparacion(self):
         pantalla.fill(self.azul)
-        pantalla.blit(self.tablero, (self.pX_tablero, self.pY_tablero))
+        pantalla.blit(self.tablero_a, (self.pX_tablero, self.pY_tablero))
         pantalla.blit(self.ayuda, (self.pX_ayuda, self.pY_ayuda))
+
+    def juego(self):
+        pantalla.fill(self.azul)
+        pantalla.blit(self.tablero_a, (self.pX_tablero, self.pY_tablero))
+        pantalla.blit(self.tablero_b, (600, 50))
 
     def instrucciones(self):
         pantalla.fill(self.azul)
@@ -122,6 +145,19 @@ class administrador_de_barcos:
         self.b4 = pygame.image.load("./assets/barcos/4_x5.png")
 
         self.barco = [self.b0, self.b1, self.b2, self.b3, self.b4]
+
+        self.fila0 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.fila1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.fila2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.fila3 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.fila4 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.fila5 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.fila6 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.fila7 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.fila8 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.fila9 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        
+        self.matriz = [self.fila0, self.fila1, self.fila2, self.fila3, self.fila4, self.fila5, self.fila6, self.fila7, self.fila8, self.fila9]
 
         self.pX = [434, 50, 386, 338, 290]
         self.pY = [434, 386, 386, 338, 290]
@@ -224,6 +260,30 @@ class administrador_de_barcos:
         self.ajustar()
         self.ubicar()
 
+    def rvs_celda(self):
+        for j in range(10):
+            for i in range(10):
+                px_clr = pantalla.get_at((51+48*i, 51+48*j))
+
+                if px_clr == (1, 74, 7, 255):
+                    self.matriz[j][i] = 1
+
+                elif px_clr == (139, 85 , 8, 255):
+                    self.matriz[j][i] = 2
+
+                elif px_clr == (17, 89, 88, 255):
+                    self.matriz[j][i] = 3
+
+                elif px_clr == (140, 144, 13, 255):
+                    self.matriz[j][i] = 4
+
+                elif px_clr == (201, 95, 45, 255):
+                    self.matriz[j][i] = 5
+
+                else:
+                    self.matriz[j][i] = 0
+            #print temporal para ver que la matriz esté en orden
+            print("{} ".format(self.matriz[j]))
 
 class administrador_de_ventanas:
     def __init__(self):
@@ -253,6 +313,13 @@ class administrador_de_ventanas:
         self.botones.preparacion()
         self.barcos.administrar()
         self.ventana = self.botones.salida
+
+    def juego(self):
+        self.imagenes.juego()
+        self.botones.juego()
+        self.barcos.ubicar()
+        self.barcos.rvs_celda()
+        self.ventana = self.botones.salida
     
     def instrucciones(self):
         self.imagenes.instrucciones()
@@ -269,6 +336,9 @@ class administrador_de_ventanas:
 
         if self.ventana == 'instrucciones':
             self.instrucciones()
+
+        if self.ventana == 'juego':
+            self.juego()
 
         if self.ventana == 'salir':
             self.salir_menu()
