ee56d4a816a853d5f4917ac1ac4de5eed7fc7b9f
madlee08 <maikemai12@hotmail.com>
2020-11-22 15:47:06 -0600

    simplicacion de tableros

--- servidor.py
+++ servidor.py
@@ -4,62 +4,57 @@ zoc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 zoc.bind(('localhost', 8080))
 
-zoc.listen(2)
+jgd_max = 4
+zoc.listen(jgd_max)
 
-print("iniciado")
-jugadores = 0
+jgd_ctd = []
+partida = []
+tableros = []
 
-fila00 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-fila01 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila02 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila03 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila04 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila05 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila06 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila07 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila08 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila09 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+for i in range(int(jgd_max/2)):
+    jgd_ctd.append(0)
+    jgd_ctd.append(0)
+    partida.append(False)
 
-tablero0 = [fila00, fila01, fila02, fila03, fila04, fila05, fila06, fila07, fila08, fila09]
+for i in range(10):
+    fila = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    tablero = []
+    for j in range(10):
+        tablero.append(fila)
+    tableros.append(tablero)
 
-fila10 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-fila11 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila12 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila13 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila14 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila15 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila16 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila17 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila18 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-fila19 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-tablero1 = [fila10, fila11, fila12, fila13, fila14, fila15, fila16, fila17, fila18, fila19]
 def hilo(cliente):
-    global jugadores
-    jugadores += 1
+    global jgd_ctd
+    num_id = -1
+    for i in range(jgd_max):
+        if jgd_ctd[i] == 0:
+            jgd_ctd[i] = 1
+            num_id = i
+            break
+    print('cliente', num_id + 1, 'conectado')
     cliente.send(str.encode("True"))
+
     booleano = 1
     while True:
-        
         if booleano != 0:
             string = cliente.recv(2048).decode("utf-8").split(".")
             for j in range(10):
                 for i in range(10):
-                    tablero0[j][i] = int(string[j][1+i*3])
-                print(tablero0[j])
+                    tableros[num_id][j][i] = int(string[j][1+i*3])
+                print(tableros[num_id][j])
+            cliente.send((str.encode(".".join(str(x) for x in tableros[num_id+1]))))
             booleano = 0
+        
         string = cliente.recv(2048).decode("utf-8")
+
         if string == 'desconectar':
             break
-    
-    print("cliente", jugadores, "ha desconectado.")
-    jugadores -= 1
+    jgd_ctd[num_id] = 0
+    print('cliente', num_id + 1, 'desconectado')
     cliente.close()
 
+print("iniciado")
 while True:
     (cliente, direccion) = zoc.accept()
+    start_new_thread(hilo, (cliente, ))
     
-    print(jugadores)
-
-    start_new_thread(hilo, (cliente,))
-
